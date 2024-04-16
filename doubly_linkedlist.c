184de9b7ed2f7ffc7c0b16b8b8c79e87610a59a3
Deepak Vashisth <146873273+vashisthd@users.noreply.github.com>
2024-04-16 11:09:12 +0530

    Add files via upload

---
+++ doubly_linkedlist.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct DLLnode{
+    int data;
+    struct DLLnode *prev,*next;
+}node;
+node* create(){
+    
+    int val;
+
+    node *start,*temp,*curr;
+    printf("Enter data : ");
+    scanf("%d",&val);
+    if(val == -1){
+        start = NULL;
+        return start;
+    }
+    start = (node*)malloc(sizeof(node));
+    start -> prev = NULL;
+    start -> data = val;
+    curr = start;
+    printf("Data Stored Successfully .....\n");
+    while(val != -1){
+        printf("Enter data : ");
+        scanf("%d",&val);
+        if(val == -1){
+            break;
+        }
+        else{
+            temp = (node*)malloc(sizeof(node));
+            temp -> data = val;
+            curr -> next = temp;
+            temp -> prev = curr;
+            curr = temp;
+            printf("Data Stored Successfully .....\n");
+        }
+    }
+    curr -> next = NULL;
+    return start;
+
+}
+void traverse(node* head){
+    node* temp = head;
+    while(temp != NULL){
+        printf("%d ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+void traverse_reverse(node* head){
+    node* temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    while(temp != NULL){
+        printf("%d ",temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+int main(){
+    node* start;
+    start = create();
+    printf("Linked list is : ");
+    traverse(start);
+    printf("Reverse Linked list is : ");
+    traverse_reverse(start);
+    return 0;
+}
